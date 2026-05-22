3330283e53ae4276cdbd101d193d576955bf2264
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-21 21:42:50 -0500

    docs(favorite-source): adjust method formatting and refine documentation for consistency and readability.

--- CatchUpPlatform.API/News/Interfaces/REST/Transform/CreateFavoriteSourceCommandFromResourceAssembler.cs
+++ CatchUpPlatform.API/News/Interfaces/REST/Transform/CreateFavoriteSourceCommandFromResourceAssembler.cs
@@ -4,17 +4,19 @@ using CatchUpPlatform.API.News.Interfaces.REST.Resources;
 namespace CatchUpPlatform.API.News.Interfaces.REST.Transform;
 
 /// <summary>
-/// Assembles a CreateFavoriteSourceCommand from a CreateFavoriteSourceResource. 
+///     Assembles a CreateFavoriteSourceCommand from a CreateFavoriteSourceResource.
 /// </summary>
 public static class CreateFavoriteSourceCommandFromResourceAssembler
 {
     /// <summary>
-    /// Assembles a CreateFavoriteSourceCommand from a CreateFavoriteSourceResource. 
+    ///     Assembles a CreateFavoriteSourceCommand from a CreateFavoriteSourceResource.
     /// </summary>
     /// <param name="resource">The CreateFavoriteSourceResource resource</param>
     /// <returns>
-    /// A CreateFavoriteSourceCommand assembled from the CreateFavoriteSourceResource
+    ///     A CreateFavoriteSourceCommand assembled from the CreateFavoriteSourceResource
     /// </returns>
-    public static CreateFavoriteSourceCommand ToCommandFromResource(CreateFavoriteSourceResource resource) =>
-        new CreateFavoriteSourceCommand(resource.NewsApiKey, resource.SourceId);  
+    public static CreateFavoriteSourceCommand ToCommandFromResource(CreateFavoriteSourceResource resource)
+    {
+        return new CreateFavoriteSourceCommand(resource.NewsApiKey, resource.SourceId);
+    }
 }
