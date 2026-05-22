164a577501ff9911ac0aa2bb6860761df84a9417
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-21 21:43:03 -0500

    docs(favorite-source): add swagger annotations and refine property descriptions for clarity.

--- CatchUpPlatform.API/News/Interfaces/REST/Resources/CreateFavoriteSourceResource.cs
+++ CatchUpPlatform.API/News/Interfaces/REST/Resources/CreateFavoriteSourceResource.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace CatchUpPlatform.API.News.Interfaces.REST.Resources;
 
 /// <summary>
-/// Represents the data required to create a favorite source. 
+///     Represents the data required to create a favorite source.
 /// </summary>
-/// <param name="NewsApiKey">The News API Key</param>
-/// <param name="SourceId">The Source ID</param>
+/// <param name="NewsApiKey">The News API Key provided by NewsAPI (required)</param>
+/// <param name="SourceId">The source identifier from NewsAPI (required)</param>
+[SwaggerSchema(Description = "Request payload to create a favorite source")]
 public record CreateFavoriteSourceResource(
-    [Required] string NewsApiKey, 
-    [Required] string SourceId);
+    [Required]
+    [SwaggerParameter(Description = "The News API Key provided by NewsAPI")] string NewsApiKey,
+    [Required]
+    [SwaggerParameter(Description = "The source identifier from NewsAPI")] string SourceId);
